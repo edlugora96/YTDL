33ed0f93c98b94fd64e29cf86bacb4e5826878ec
Eduardo Gonzalez <edlu.gora96@gmail.com>
2019-10-16 19:23:02 -0400

    corrections in the thumbs creator module

--- gora-api/YTDL/index.js
+++ gora-api/YTDL/index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const config = require("config")();
 const ytdl = require("ytdl-core");
 var ffmpeg = require("ffmpeg");
+const ThumbnailGenerator = require("video-thumbnail-generator").default;
 
 const ytdlApi = app => {
   const router = express.Router();
@@ -12,31 +13,31 @@ const ytdlApi = app => {
   router.post("/", async (req, res, next) => {
     const { body: data } = req;
     try {
-      await ytdl(data.url)
+      await ytdl(data.url, {
+        quality: "highest"
+      })
         .on("progress", (length, downloaded, totallength) => {
+          console.log({ length, downloaded, totallength });
           if (downloaded === totallength) {
-            console.log({ length, downloaded, totallength });
-            const loadVideo = new ffmpeg(
-              path.join(__dirname, `../../uploads/${data.name}.mp4`)
-            );
-            loadVideo.then(
-              async video => {
-                const name = await video.fnExtractFrameToJPG(
-                  path.join(__dirname, "../../uploads/thumbs"),
-                  {
-                    number: 3,
-                    every_n_percentage: "30%",
-                    file_name: data.name,
-                    start_time: 0,
-                    duration_time: 1
-                  }
-                );
-                console.log(name);
-              },
-              err => {
-                next(...err);
-              }
-            );
+            const tg = new ThumbnailGenerator({
+              sourcePath: path.join(
+                __dirname,
+                `../../uploads/${data.name}.mp4`
+              ),
+              thumbnailPath: path.join(__dirname, "../../uploads/thumbs/")
+            });
+            tg.generateOneByPercent(10, {
+              size: "100%",
+              filename: `${data.name}_1`
+            }).then(console.log);
+            tg.generateOneByPercent(50, {
+              size: "100%",
+              filename: `${data.name}_2`
+            }).then(console.log);
+            tg.generateOneByPercent(90, {
+              size: "100%",
+              filename: `${data.name}_3`
+            }).then(console.log);
           }
         })
         .pipe(await fs.createWriteStream(`uploads/${data.name}.mp4`));
